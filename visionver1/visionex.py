0a3f969332b7496eed6f6133c5211ba744490d1b
paolopoli1980 <paolopoli1980@gmail.com>
2016-10-16 10:44:11 +0200

    Add files via upload

---
+++ visionver1/visionex.py
@@ -0,0 +1,109 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 19 19:02:44 2016
+
+@author: paolo
+"""
+
+from pylab import*
+import numpy as np
+from PIL import Image
+from random import randint
+import math
+
+def ex1():
+    img= Image.open('imex7.png')
+    img = img.convert('L')
+    im = img.load()
+    img.show()
+    numbercolor=255
+    listpoint=[]
+    number=200
+    ntry=5
+    xmax=0
+    ymax=0
+ 
+    el=0
+    for k in range(number):
+        for i in range(img.size[0]):
+            for j in range(img.size[1]):
+                if im[i,j]!=numbercolor and im[i,j]!=0 and im[i,j]!=1:    
+                    listpoint.append([i,j])
+                   # print im[i,j]    
+                   # print i
+                #print j    
+        for i in range(img.size[0]):
+            for  j in range(img.size[1]):
+                if im[i,j]==numbercolor:
+                    try:
+                        if im[i-1,j]<numbercolor and im[i+1,j]<numbercolor and im[i,j+1]<numbercolor and im[i,j-1]<numbercolor:
+                            im[i,j]=1
+                    except:
+                        pass
+        perc=img.size[0]*img.size[1]/10000
+        print perc    
+        if len(listpoint)<=perc:
+            break
+        if len(listpoint)>perc:
+            casual=randint(0,len(listpoint))
+           #print listpoint
+            el+=1
+            
+            x=listpoint[casual][0]
+            y=listpoint[casual][1]
+ 
+            alfainc=0
+ 
+            for t in range(ntry):
+                massimo=0
+ 
+                xnew=x
+                ynew=y
+                if t>0:
+                    x=(xmax+x)/2
+                    y=(ymax+y)/2
+                    alfainc=0
+    
+                while alfainc<6.29:
+                    alfainc+=0.005
+ 
+                    xnew=x
+                    ynew=y
+                    
+                    try:
+                        while im[xnew,int(ynew)]<numbercolor:
+                           # print im[xnew,int(ynew)]
+                            #print "ostrega"
+                            xnew=xnew+cos(alfainc)
+                            #ynew+=1
+                            ynew=ynew+sin(alfainc)
+                            dist=math.sqrt((x-xnew)**2+(y-ynew)**2)
+                            if dist>massimo:
+                                massimo=dist
+                                xmax=xnew
+                                ymax=ynew
+                                
+                            try:
+                                if im[xnew,int(ynew)]!=numbercolor:
+                                    #print "cacchio"
+                                    im[xnew,int(ynew)]=1
+                            except:
+                                pass
+                    except:
+                        pass                
+            print "finish"    
+            listpoint=[]    
+            print el       
+        
+    
+                  
+ 
+
+ 
+    print listpoint
+    elem.append(el)             
+    img.show() 
+elem=[]
+for i in range(1):
+    ex1()
+print elem    
